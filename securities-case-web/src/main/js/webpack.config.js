a3111b7212555e45565b89aac913eaffcae3131e
Srirangan <srirangan@gmail.com>
2016-03-18 17:45:04 +0100

    hello world dependency management + build setup with webpack, es6, babel and react

---
+++ securities-case-web/src/main/js/webpack.config.js
@@ -0,0 +1,22 @@
+var path = require('path');
+var webpack = require('webpack');
+
+module.exports = {
+    entry: './index.js',
+    output: {
+        path: path.resolve(__dirname, '..', 'webapp'),
+        filename: 'bundle.js'
+    },
+    module: {
+        loaders: [
+            {
+                test: /.js?$/,
+                loader: 'babel-loader',
+                exclude: /node_modules/,
+                query: {
+                    presets: ['es2015', 'react']
+                }
+            }
+        ]
+    }
+};
