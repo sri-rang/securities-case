25497a6a033a05e4f8ba343c6c1833598d95a76c
Srirangan <srirangan@gmail.com>
2016-03-20 15:57:35 +0100

    move npm stuff to web-client module root

--- securities-case-web-client-prototype/webpack.config.js
+++ securities-case-web-client-prototype/webpack.config.js
@@ -2,9 +2,9 @@ var path = require('path');
 var webpack = require('webpack');
 
 module.exports = {
-    entry: './index.js',
+    entry: './src/main/js/index.js',
     output: {
-        path: path.resolve(__dirname, '../../../../securities-case-web/src/main/webapp'),
+        path: path.resolve(__dirname, '../securities-case-web/src/main/webapp'),
         filename: 'bundle.js'
     },
     module: {
