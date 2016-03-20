893ab2b316e0f2ba46819351142192e34f777929
Srirangan <srirangan@gmail.com>
2016-03-20 17:54:27 +0100

    code cleanup

--- securities-case-web-client-prototype/src/main/js/reducers/filter_instruments.js
+++ securities-case-web-client-prototype/src/main/js/reducers/filter_instruments.js
@@ -3,14 +3,13 @@ import actions from '../actions';
 const default_state = {instruments: [], selected: null};
 
 const reducer = (state = default_state, action) => {
-    if (action.type === actions.types.get_instruments) {
-        return Object.assign({}, state, {instruments: [{code: null}, ...action.instruments]});
-    }
-    else if (action.type === actions.types.select_instrument) {
-        return Object.assign({}, state, {selected: action.code});
-    }
-    else {
-        return state;
+    switch (action.type) {
+        case actions.types.get_instruments:
+            return Object.assign({}, state, {instruments: [{code: null}, ...action.instruments]});
+        case actions.types.select_instrument:
+            return Object.assign({}, state, {selected: action.code});
+        default:
+            return state;
     }
 };
 
--- securities-case-web-client-prototype/src/main/js/reducers/messages.js
+++ securities-case-web-client-prototype/src/main/js/reducers/messages.js
@@ -3,15 +3,14 @@ import actions from '../actions';
 const default_state = {messages: [], selected: null};
 
 const reducer = (state = default_state, action) => {
-    if (action.type === actions.types.get_message) {
-        const messages = [action.message, ...state.messages];
-        return Object.assign({}, state, {messages});
-    }
-    else if (action.type === actions.types.select_instrument) {
-        return Object.assign({}, state, {selected: action.code});
-    }
-    else {
-        return state;
+    switch (action.type) {
+        case actions.types.get_message:
+            const messages = [action.message, ...state.messages];
+            return Object.assign({}, state, {messages});
+        case actions.types.select_instrument:
+            return Object.assign({}, state, {selected: action.code});
+        default:
+            return state;
     }
 };
 
